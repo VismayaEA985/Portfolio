8e2ac6018109a4fd63acc60edb78de2c351a33e2
VismayaEA985 <eavismaya985@gmail.com>
2026-09-20 20:11:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage";
+import SkillsPage from "./pages/SkillsPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectDetail from "./pages/ProjectDetail";
+import ContactPage from "./pages/ContactPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "background.default",
+        }}
+      >
+        <Navbar />
+
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/skills" element={<SkillsPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/project/:id" element={<ProjectDetail />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Box>
+
+        <Footer />
+      </Box>
+    </BrowserRouter>
+  );
+}
